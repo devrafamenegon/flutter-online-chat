1c2bc4d3dfb8151ad062e565c8bd5ab7ceb068b6
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-02-05 22:44:56 -0300

    Sending text to the Firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chat/chat_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  
-  FirebaseFirestore.instance.collection('mensagens').doc().set({
 
-  });
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
